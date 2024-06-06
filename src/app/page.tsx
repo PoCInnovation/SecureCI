2d45768c5a3741f4f492f22235b449a9f841810f
Sephorah ANIAMBOSSOU <sephorah.aniambossou@epitech.eu>
2024-06-06 11:22:33 +0200

    feat: init project with next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import Typography from '@mui/material/Typography';
+
+const Home: NextPage = () => {
+  return (
+    <Typography variant="h2">Secure CI</Typography>
+  );
+}
+
+export default Home;
