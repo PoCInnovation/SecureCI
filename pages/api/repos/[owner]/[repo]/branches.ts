5cd60262aa9486050f40879f3fff0c820fcc9bd5
Quentin <quentin.bollore@epitech.eu>
2024-06-29 01:55:51 +0200

    feat(branches): api call for branches

---
+++ pages/api/repos/[owner]/[repo]/branches.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const session = await getSession({ req });
+
+    if (!session) {
+      res.status(401).json(
+        { message: "Unauthorized" }
+    );
+      return;
+    }
+
+    const { owner, repo } = req.query;
+
+    const apiUrl: string = `https://api.github.com/repos/${owner}/${repo}/branches`;
+
+    const response = await fetch(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${session.accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch branches");
+    }
+
+    const branches = await response.json();
+    res.status(200).json(branches);
+  } catch (error) {
+    res.status(500).json(
+        { message: "Failed to fetch branches" }
+    );
+  }
+};
+
+export default handler;
