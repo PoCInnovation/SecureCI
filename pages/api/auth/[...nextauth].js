4e177767bee26cccb753ccb3c1e80157bf962003
Quentin <quentin.bollore@epitech.eu>
2024-06-28 15:24:53 +0200

    feat(api): async function to get access token from github

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,12 +5,28 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
     // ...add more providers here
   ],
-  secret: process.env.JWT_SECRET
+  secret: process.env.JWT_SECRET,
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      if (account) {
+        token.accessToken = account.access_token
+        token.id = profile.id
+      }
+      return token
+    },
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken
+      session.user.id = token.id
+      console.log(session, token, user);
+      
+      return session
+    }
+  }
 }
 
 export default NextAuth(authOptions)
