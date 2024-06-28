77595168e80ab2bb62e5ad6f5d03214de413b82b
Quentin <quentin.bollore@epitech.eu>
2024-06-29 01:57:00 +0200

    feat(issues): api call to get issues from repository

---
+++ pages/api/repos/[owner]/[repo]/issues.ts
@@ -0,0 +1,39 @@
+// pages/api/repos/[owner]/[repo]/issues.ts
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const session : any = await getSession({ req });
+
+    if (!session) {
+      res.status(401).json(
+        { message: "Unauthorized" }
+    );
+      return;
+    }
+
+    const { owner, repo } = req.query;
+
+    const apiUrl : string = `https://api.github.com/repos/${owner}/${repo}/issues`;
+
+    const response = await fetch(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${session.accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch issues");
+    }
+
+    const issues : object = await response.json();
+    res.status(200).json(issues);
+  } catch (error) {
+    res.status(500).json(
+        { message: "Failed to fetch issues" }
+    );
+  }
+};
+
+export default handler;
