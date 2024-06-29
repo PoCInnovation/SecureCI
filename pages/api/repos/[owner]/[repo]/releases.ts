a98916d019ae5c1c7a7967c19ba1e5b9e5f33ab4
Quentin <quentin.bollore@epitech.eu>
2024-06-29 21:50:37 +0200

    feat(api): add route to get releases

---
+++ pages/api/repos/[owner]/[repo]/releases.ts
@@ -0,0 +1,55 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import { getSession } from "next-auth/react";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        const session = await getSession({ req });
+
+        if (!session) {
+            res.status(401).json(
+                { message: "Unauthorized" }
+            );
+            return;
+        }
+
+        const { 
+            owner,
+            repo 
+        } = req.query;
+
+        if (!owner || !repo) {
+            res.status(400).json(
+                { message: "Invalid query" }
+            );
+            return;
+        }
+
+        if (typeof owner !== "string" || typeof repo !== "string") {
+            res.status(400).json(
+                { message: "Invalid type query" }
+            );
+            return;
+        }
+
+        const apiUrl: string = `https://api.github.com/repos/${owner}/${repo}/releases`;
+
+        const response = await fetch(apiUrl, {
+            headers: {
+                Authorization: `Bearer ${session.accessToken}`,
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch releases");
+        }
+
+        const releases: object = await response.json();
+        res.status(200).json(releases);
+    } catch (error) {
+        res.status(500).json(
+            { message: "Failed to fetch releases" }
+        );
+    }
+}
+
+export default handler;
