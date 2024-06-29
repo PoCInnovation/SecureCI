2f1784979e48e5a498cbe6a968dab1fef4720edd
Quentin <quentin.bollore@epitech.eu>
2024-06-29 21:46:06 +0200

    feat(api): add route to get contributors from a repo

---
+++ pages/api/repos/[owner]/[repo]/contributors.ts
@@ -0,0 +1,51 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const session = await getSession({ req });
+
+    if (!session) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+
+    const { 
+        owner, 
+        repo 
+    } = req.query;
+
+    if (!owner || !repo) {
+      res.status(400).json(
+        { message: "Invalid query" }
+    );
+      return;
+    }
+
+    if (typeof owner !== "string" || typeof repo !== "string") {
+      res.status(400).json(
+        { message: "Invalid type query" }
+    );
+      return;
+    }
+
+    const apiUrl: string = `https://api.github.com/repos/${owner}/${repo}/contributors`;
+
+    const response = await fetch(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${session.accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch contributors");
+    }
+
+    const contributors: object = await response.json();
+    res.status(200).json(contributors);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch contributors" });
+  }
+};
+
+export default handler;
