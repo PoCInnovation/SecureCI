cc473647e2672acb609d4334c1b1078cfbd80deb
Sephorah ANIAMBOSSOU <sephorah.aniambossou@epitech.eu>
2024-07-11 19:17:43 +0200

    fix(dashboard): side bar attributes

--- src/components/auth/auth-button.tsx
+++ src/components/auth/auth-button.tsx
@@ -1,8 +1,6 @@
 "use client";
-import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "../ui/buttons/button";
-import { redirect } from "next/navigation";
 
 const AuthButton = () => {
     const { data: session } = useSession();
@@ -18,7 +16,9 @@ const AuthButton = () => {
     return (
         <>
             <p>Log in with Github</p><br/>
-            <Button onClick={() => { signIn();}}>Sign In</Button>
+            <Button onClick={() => { signIn('github', { callbackUrl: "/dashboard"}); }}>
+                Sign In
+            </Button>
         </>
     )
 }
--- src/components/ui/side-bar.tsx
+++ src/components/ui/side-bar.tsx
@@ -1,16 +1,12 @@
 "use client"
-import { Link } from "lucide-react"
-import { buttonVariants } from "./buttons/button"
-import { ButtonLink } from "./buttons/button-link"
+import { LogOut, Plus, User } from "lucide-react"
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
-    DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "./dropdown-menu/dropdown-menu"
-import { useState } from "react"
 import { signOut, useSession } from "next-auth/react"
 
 
@@ -27,13 +23,13 @@ const SideBar = ({ organizations, currentOrg }: SideBarChildrenProps) => {
     const { data: session } = useSession();
 
     return (
-        <div class="h-screen w-screen bg-white dark:bg-slate-900">
-            <aside id="sidebar" class="fixed left-0 top-0 z-40 h-screen w-64 transition-transform" aria-label="Sidebar">
-                <div class="flex h-full flex-col overflow-y-auto border-r border-slate-200 bg-white px-3 py-4 dark:border-slate-700 dark:bg-slate-900">
-                    <div href="#" class="mb-10 flex items-center rounded-lg px-3 py-2 text-slate-900 dark:text-white">
+        <div className="h-screen w-screen bg-white dark:bg-slate-900">
+            <aside id="sidebar" className="fixed left-0 top-0 z-40 h-screen w-64 transition-transform" aria-label="Sidebar">
+                <div className="flex h-full flex-col overflow-y-auto border-r border-slate-200 bg-white px-3 py-4 dark:border-slate-700 dark:bg-slate-900">
+                    <div className="mb-10 flex items-center rounded-lg px-3 py-2 text-slate-900 dark:text-white">
                         <svg width="24" height="19" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <g clip-path="url(#clip0_901_948)">
-                                <path d="M21 28V2C21 1.447 20.553 1 20 1H2C1.447 1 1 1.447 1 2V31H8V25H14V31H31V8C31 8 31 7 30 7H24M16 6V8M26 12V14M26 18V20M11 6V8M6 6V8M16 12V14M11 12V14M6 12V14M16 18V20M11 18V20M6 18V20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <g clipPath="url(#clip0_901_948)">
+                                <path d="M21 28V2C21 1.447 20.553 1 20 1H2C1.447 1 1 1.447 1 2V31H8V25H14V31H31V8C31 8 31 7 30 7H24M16 6V8M26 12V14M26 18V20M11 6V8M6 6V8M16 12V14M11 12V14M6 12V14M16 18V20M11 18V20M6 18V20" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                             </g>
                             <defs>
                                 <clipPath id="clip0_901_948">
@@ -42,19 +38,27 @@ const SideBar = ({ organizations, currentOrg }: SideBarChildrenProps) => {
                             </defs>
                         </svg>
                         <DropdownMenu>
-                            <DropdownMenuTrigger class="ml-3 text-base font-semibold">{currentOrg.name}</DropdownMenuTrigger>
+                            <DropdownMenuTrigger className="ml-3 text-base font-semibold">{currentOrg.name}</DropdownMenuTrigger>
                             <DropdownMenuContent>
                                 {organizations?.map(org => {
                                     return (
                                         <DropdownMenuItem key={org.name}>
-                                            <span class="w-56">{org.name}</span>
+                                            <span className="w-56">{org.name}</span>
                                             </DropdownMenuItem>
                                     )
                                 })}
-                                <DropdownMenuSeparator />
-                                <DropdownMenuItem>{session?.user?.name}</DropdownMenuItem>
                                 <DropdownMenuItem>
-                                    <p onClick={() => { signOut() }}>Log out</p>
+                                    <Plus className="mr-2 h-4 w-4"/>
+                                    <span>Add org</span>
+                                </DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>
+                                    <User className="mr-2 h-4 w-4" />
+                                    {session?.user?.name}
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <LogOut className="mr-2 h-4 w-4" />
+                                    <span onClick={() => { signOut() }}>Log out</span>
                                 </DropdownMenuItem>
                             </DropdownMenuContent>
                         </DropdownMenu>
