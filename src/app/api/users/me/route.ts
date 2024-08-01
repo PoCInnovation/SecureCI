cac83a0da2fef05f7adde650391df1bf4e4e3bf1
Quentin <quentin.bollore@epitech.eu>
2024-08-01 19:35:11 +0200

    feat(api): code to change settings of user (no test yet)

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -17,7 +17,7 @@ export async function GET(
 
         const apiUrl: string = "https://api.github.com/user";
 
-        const response: any = await fetchURL(req, apiUrl);
+        const response: any = await fetchURL(req, apiUrl, "GET");
 
         const user: object = await response.json();
 
@@ -28,6 +28,52 @@ export async function GET(
     } catch (error) {
         return NextResponse.json(
             { message: "Failed to fetch user" },
+            { status: 500 }
+        );
+    }
+};
+
+export async function PATCH(
+    req: NextRequest,
+) {
+    try {
+        const session: any = await getServerSession({ req, ...authOptions });
+        if (!session) {
+            return NextResponse.json(
+                { message: "Unauthorized" },
+                { status: 401 }
+            );
+        }
+
+        const apiUrl: string = "https://api.github.com/user";
+
+        const body = await req.json();
+        const response: any = await fetch(apiUrl, {
+            method: 'PATCH',
+            headers: {
+                'Authorization': `Bearer ${session.accessToken}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            return NextResponse.json(
+                { message: "Failed to update user", error: errorData },
+                { status: response.status }
+            );
+        }
+
+        const updatedUser: object = await response.json();
+
+        return NextResponse.json(
+            updatedUser,
+            { status: 200 }
+        );
+    } catch (error) {
+        return NextResponse.json(
+            { message: "Failed to update user" },
             { status: 500 }
         );
     }
