5575794d2d726f1747c5a3f80ad5ac706fa30df3
Quentin <quentin.bollore@epitech.eu>
2024-08-08 17:44:06 +0200

    feat(login-page): theme / dark mode

---
+++ src/components/auth/theme-context.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.documentElement.setAttribute(
+      "color-scheme",
+      darkMode ? "light" : "dark"
+    );
+  };
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
