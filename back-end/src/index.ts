966f6cc059bc2e7a4c6d1496301a562bf305bcc6
Sephorah ANIAMBOSSOU <sephorah.aniambossou@epitech.eu>
2024-09-20 00:05:29 +0200

    feat: rename .sarif file according to its repository and repo owner, and store .sarif file for each repository in semgrep-scan-results-extracted directory

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response, Application } from 'express';
 import { appId, privateKeyPath, serverPort, webhookSecret, clientSecret, clientId } from './config.js';
 import { App } from 'octokit';
 import { createNodeMiddleware } from '@octokit/webhooks';
-import { createWriteStream, readFileSync, rm } from 'fs';
+import { createWriteStream, existsSync, mkdir, mkdirSync, readFileSync, rename, rm } from 'fs';
 import { createServer } from "node:http";
 import { Readable } from "stream";
 import AdmZip from "adm-zip";
@@ -33,37 +33,44 @@ const getSemgrepScanResults = async ({ octokit, payload }) => {
                 'X-GitHub-Api-Version': '2022-11-28'
             }
         });
-        const artifact = response.data.artifacts[0];
-        const downloadedArtifact = await octokit.request(
-            'GET /repos/{owner}/{repo}/actions/artifacts/{artifact_id}/{archive_format}', {
-            owner: payload.repository.owner.login,
-            repo: payload.repository.name,
-            artifact_id: artifact.id,
-            archive_format: 'zip',
-            headers: {
-              'X-GitHub-Api-Version': '2022-11-28'
-            }
-        })
-        const fileName = "semgrep-scan-results";
-        const outputDirectory = `${fileName}_extracted`;
-        rm(outputDirectory, { 
-            recursive: true, 
-          }, (error) => { 
-            if (error) { 
-              console.log(error); 
-            } else { 
-              console.log("Deleted old scan results");
-            } 
-          }); 
-        const artifactZipFile = await fetch(downloadedArtifact.url);
-        if (artifactZipFile.ok && artifactZipFile.body) {
-            console.log("Writing to file:", fileName);
-            let writer = createWriteStream(fileName);
-            Readable.fromWeb(artifactZipFile.body).pipe(writer);
+        const outputDirectory = `semgrep-scan-results-extracted`;
+        if (!existsSync(outputDirectory)){
+            mkdirSync(outputDirectory);
         }
-        const zip = new AdmZip(fileName);
-        zip.extractAllTo(outputDirectory);
-        console.log(`Extracted to "${outputDirectory}" successfully`);
+        for (const artifact of response.data.artifacts) {
+            const downloadedArtifact = await octokit.request(
+                'GET /repos/{owner}/{repo}/actions/artifacts/{artifact_id}/{archive_format}', {
+                owner: payload.repository.owner.login,
+                repo: payload.repository.name,
+                artifact_id: artifact.id,
+                archive_format: 'zip',
+                headers: {
+                    'X-GitHub-Api-Version': '2022-11-28'
+                }
+            })
+            const fileName: string = artifact.name;
+            const artifactZipFile = await fetch(downloadedArtifact.url);
+            if (artifactZipFile.ok && artifactZipFile.body) {
+                let writer = createWriteStream(fileName);
+                await new Promise((resolve, reject) => {
+                    Readable.fromWeb(artifactZipFile.body).pipe(writer);
+                    writer.on('finish', resolve);
+                    writer.on('error', reject);
+                });
+                console.log("Writing to file:", fileName);
+                const zip = new AdmZip(fileName);
+                zip.extractAllTo(outputDirectory);
+                console.log(`Extracted to "${outputDirectory}" successfully`);
+                rename(
+                    `${outputDirectory}/semgrep-results.sarif`,
+                    `${outputDirectory}/${fileName}.sarif`,
+                    () => {
+                        console.log(`\nFile renamed, from ${outputDirectory}/semgrep-results.sarif
+                            to ${outputDirectory}/${fileName}.sarif!\n`);
+                    });
+            }
+        }
+        // const fileName = "semgrep-scan-results";
     } catch (error) {
         console.log(error)
     }
